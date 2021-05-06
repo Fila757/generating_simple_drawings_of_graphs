94d732cc58135272c98293ddf9af16f1c93abdc3
fila <filip.cermak2@gmail.com>
2021-05-06 16:37:05 +0200

    linux version
    Former-commit-id: 0a1336bbd4d057c300187920dd8d984082cbeece

--- coordinates_generator/drawing_of_cliques.cpp
+++ coordinates_generator/drawing_of_cliques.cpp
@@ -11,35 +11,34 @@ int main()
 {
     int n = 8;
 
-    //cout << "number of thrds " << std::thread::hardware_concurrency() << endl;
+    cout << "number of thrds " << std::thread::hardware_concurrency() << endl;
 
-    int number_of_threads = 1;
+    int number_of_threads = std::thread::hardware_concurrency();
 
-    //vector<graph> graphs;
-    // shared_ptr<canonic_wraper> shared_wraper = make_shared<canonic_wraper>();
+    vector<graph> graphs;
      
-    //system(("split --numeric-suffixes=1 --additional-suffix=.txt -l" +to_string( 5370725 / (number_of_threads - 1) + 1) +  " data/graph" + to_string(n) + ".txt data/graph" + to_string(n) + "_").data());
+    system(("split -d --additional-suffix=.txt -l" +to_string( 5370725 / (number_of_threads - 1) + 1) +  " data/graph" + to_string(n) + ".txt data/graph" + to_string(n) + "_").data());
     
-    //for(int i = 0; i < number_of_threads - 1;i++){
-    //	graphs.push_back(graph(n, i+1));
-    //}
-    //cout << "graphs created" << endl;
+    for(int i = 0; i < number_of_threads - 1;i++){
+    	graphs.push_back(graph(n, i));
+    }
+    cout << "graphs created" << endl;
     
-    auto g = graph(n, 1);
-    g.create_all_possible_drawings();
-    //vector<thread> threads;
-    //for(int i = 0; i < number_of_threads - 1;i++){
-    //	threads.push_back(thread(&graph::create_all_possible_drawings, &graphs[i]));
-    //}
+    //auto g = graph(n, 1);
+    //g.create_all_possible_drawings();
+    vector<thread> threads;
+    for(int i = 0; i < number_of_threads - 1;i++){
+    	threads.push_back(thread(&graph::create_all_possible_drawings, &graphs[i]));
+    }
     
-    //cout << "after run" << endl;
-    //for(int i = 0; i < number_of_threads - 1;i++)
-    //   threads[i].join();
+    cout << "after run" << endl;
+    for(int i = 0; i < number_of_threads - 1;i++)
+       threads[i].join();
     
-    //system(("cat ../VizualizerWPF/data/graph" + to_string(n) + "_* > ../VizualizerWPF/data/graph" + to_string(n) + ".txt").data());
+    system(("cat ../VizualizerWPF/data/graph" + to_string(n) + "_* > ../VizualizerWPF/data/graph" + to_string(n) + ".txt").data());
                                                                          
-    //system("rm data/*_*");
-    //system("rm ../VizualizerWPF/data/*_*");
+    system("rm data/*_*");
+    system("rm ../VizualizerWPF/data/*_*");
      
 }
 
