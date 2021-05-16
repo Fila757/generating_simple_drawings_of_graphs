08b57ab712800e53c2b69daaa59eb3bf77be3174
filip.cermak2@gmail.com <filip.cermak2@gmail.com>
2021-05-16 10:55:51 +0200

    commented collision detection (minor changes to mainwindow xaml)
    Former-commit-id: 190ec65c8dd5d23eeabc9fd5671f58c188d88bdc

--- VizualizerWPF/CollisionDetection.cs
+++ VizualizerWPF/CollisionDetection.cs
@@ -9,12 +9,19 @@ using Path = System.Windows.Shapes.Path;
 namespace VizualizerWPF
 {
 
+    /// <summary>
+    /// Line in form ax + by + c = 0
+    /// </summary>
     class LineWithCoeffients
     {
         public double a;
         public double b;
         public double c;
     }
+
+    /// <summary>
+    /// Half line with the direction which is either the same or opposite to "line", the "direction" choose which one.
+    /// </summary>
 
     class HalfLineWithCoeffients
     {
@@ -39,12 +46,17 @@ namespace VizualizerWPF
         }
 
         /// <summary>
-        /// Error
+        /// Error rate
         /// </summary>
         static double epsilon = 0.00001;
 
 
-
+        /// <summary>
+        /// Function to detect whether Line and HalfLine intersect. 
+        /// </summary>
+        /// <param name="line1"></param>
+        /// <param name="halfLine"></param>
+        /// <returns></returns>
         public static Point? LineAndHalfLine (Line line1, HalfLineWithCoeffients halfLine)
         {
             var line2 = halfLine.line;
@@ -74,22 +86,6 @@ namespace VizualizerWPF
             {
                 throw new ArgumentException("Intersects point");
             }
-
-            /*
-            if (debug)
-            {
-                window.mainCanvas.Children.Add(new Ellipse
-                {
-                    Width = window.sizeOfVertex,
-                    Height = window.sizeOfVertex,
-                    Fill = Brushes.Lime,
-                    Margin = new Thickness(
-                        new Point { X = line1.X1 + (line1.X2 - line1.X1) * t, Y = line1.Y1 + (line1.Y2 - line1.Y1) * t }.X - window.sizeOfVertex / 2,
-                        new Point { X = line1.X1 + (line1.X2 - line1.X1) * t, Y = line1.Y1 + (line1.Y2 - line1.Y1) * t }.Y - window.sizeOfVertex / 2, 0, 0)
-
-                });
-            }
-            */
 
             if (t >= -0.00001 && t <= 1.00001) //line1 is just segments so we need to check limits of s
                 return new Point { X = line1.X1 + (line1.X2 - line1.X1) * t, Y = line1.Y1 + (line1.Y2 - line1.Y1) * t };
@@ -123,6 +119,13 @@ namespace VizualizerWPF
             return new Point { X = -1, Y = -1 };
         }
 
+
+        /// <summary>
+        /// Detect if two lines intersect except of their end points and return the intersection
+        /// </summary>
+        /// <param name="line1"></param>
+        /// <param name="line2"></param>
+        /// <returns></returns>
         public static Point TwoLinesIntersectNotAtTheEnd(Line line1, Line line2)
         {
             var denominator = (line2.Y2 - line2.Y1) * (line1.X2 - line1.X1) - (line2.X2 - line2.X1) * (line1.Y2 - line1.Y1);
@@ -141,7 +144,12 @@ namespace VizualizerWPF
             return new Point { X = -1, Y = -1 };
         }
 
-
+        /// <summary>
+        ///  Detect if two lines intersect except of their end points
+        /// </summary>
+        /// <param name="line1"></param>
+        /// <param name="line2"></param>
+        /// <returns></returns>
         public static bool CheckIfTwoLinesIntersectNotAtTheEnd(Line line1, Line line2)
         {
             var denominator = (line2.Y2 - line2.Y1) * (line1.X2 - line1.X1) - (line2.X2 - line2.X1) * (line1.Y2 - line1.Y1);
@@ -160,6 +168,11 @@ namespace VizualizerWPF
             return false;
         }
 
+        /// <summary>
+        /// Function to detect whether conrete <c>line</c> intersect any of the lines of the window
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
         public static bool IntersectsSomeLine(Line line)
         {
             int numberOfIntersection = 0;
@@ -216,7 +229,12 @@ namespace VizualizerWPF
                 return true;
             return false;
         }
-
+        /// <summary>
+        /// Function to detect if vertex <c>v</c> lines on <c>line</c>.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="v"></param>
+        /// <returns></returns>
         public static bool CenterOfVertexOnLine(Line line, Vertex v)
         {
             var center = v.center;
@@ -237,7 +255,12 @@ namespace VizualizerWPF
             return false;
         }
 
-
+        /// <summary>
+        /// Detects if a point <c>center</c> lies on line <c>line</c>
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="center"></param>
+        /// <returns></returns>
         public static bool CenterOnLine(Line line, Point center)
         { 
             if (((line.Y1 - line.Y2) * center.X + (line.X2 - line.X1) * center.Y
@@ -309,12 +332,24 @@ namespace VizualizerWPF
             return false;
         }
 
+        /// <summary>
+        /// return point on <c>line</c> in ratio <c>a</c> to <c>b</c>.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
 
         static Point GetAlmostMid(Line line, int a, int b)
         {
             return new Point((a * line.X1 + b * line.X2) / (a+b), (a * line.Y1 + b * line.Y2) / (a+b)); 
         }
 
+        /// <summary>
+        /// Convert <c>line</c> into LineWithCoefficients 
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
 
         static LineWithCoeffients GetLineWithCoefficients(Line line)
         {
@@ -325,20 +360,20 @@ namespace VizualizerWPF
             };
         }
 
-        /*
-        static LineWithCoeffients GetLineWithCoeffientsByPointAndVector(Point point, Vector v)
-        {
-            double c = -(v.X * point.X + v.Y * point.Y);
-
-            return new LineWithCoeffients { a = v.X, b = v.Y, c = c };
-        }
-        */
-
+        
 
         public static double GetDistance(Line line)
         {
             return Math.Sqrt((line.X2 - line.X1) * (line.X2 - line.X1) + (line.Y2 - line.Y1) * (line.Y2 - line.Y1));
         }
+
+        /// <summary>
+        /// Create a perpendicular line to <c>line</c>t at the point which has ration <c>a</c> to <c>b</c>.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
 
         static (HalfLineWithCoeffients, HalfLineWithCoeffients) GetPerpendicularToAlmostMid(Line line, int a, int b) {
             var mid = GetAlmostMid(line, a, b);
@@ -367,7 +402,11 @@ namespace VizualizerWPF
             return (leftHalfLine, rightHalfLine);
         }
 
-
+        /// <summary>
+        /// Create many of rays going from <c>point</c>.
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
         public static IEnumerable<HalfLineWithCoeffients> GetRaysGoingFromPoint(Point point)
         {
             var directions = new Vector[] {
@@ -391,6 +430,11 @@ namespace VizualizerWPF
 
         }
 
+        /// <summary>
+        /// Iterator for getting many perpendicular lines to <c>line</c>
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
         public static IEnumerable<HalfLineWithCoeffients> GetPerpendicularsToAlmostMids(Line line)
         {
             for (int i = 0; i < 5; i++)
@@ -402,7 +446,13 @@ namespace VizualizerWPF
             
         }
 
-
+        /// <summary>
+        /// Function to detect the orieantion of the triangle formed by <c>lines</c> and <c>line</c>, where <c>point</c> determines the reference face
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="lines"></param>
+        /// <param name="point"></param>
+        /// <returns></returns>
         public static int GetOrientation(Line line, List<Line> lines, Point point)
         {
             bool isInside = false;
@@ -493,16 +543,23 @@ namespace VizualizerWPF
             return -1;
         } 
 
-        static Line MakeReversedLine(Line line)
-        {
-            return new Line { X1 = line.X2, Y1 = line.Y2, X2 = line.X1, Y2 = line.Y1 };
-        }
-
+        /// <summary>
+        /// Function to get opposite end of <c>edge</c> to <c>point</c>.
+        /// </summary>
+        /// <param name="edge"></param>
+        /// <param name="point"></param>
+        /// <returns></returns>
         public static Point ChooseOppositeOne(Edge edge, Point point)
         {
             return Vertex.Compare(edge.points[0], point) ? edge.points.Last() : edge.points[0];
         }
 
+        /// <summary>
+        /// Function to get line of Edge <c>e</c> incident to  <c>v</c>.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="e"></param>
+        /// <returns></returns>
         public static Line ChooseTheLineBy(Vertex v, Edge e)
         {
             Line l = Vertex.Compare(e.points[0], v.center) ? e.lines[0] : e.lines.Last();
@@ -510,7 +567,12 @@ namespace VizualizerWPF
                 return l;
             return new Line { X1 = l.X2, Y1 = l.Y2, X2 = l.X1, Y2 = l.Y1 }; //swapping the orientation
         }
-
+        /// <summary>
+        /// Function to orient line <c>l</c> so that <c>v</c> is first point.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="l"></param>
+        /// <returns></returns>
         public static Line OrientLineProperly(Vertex v, Line l)
         {
             if (Vertex.Compare(new Point(l.X1, l.Y1), v.center))
@@ -518,7 +580,13 @@ namespace VizualizerWPF
             return new Line { X1 = l.X2, Y1 = l.Y2, X2 = l.X1, Y2 = l.Y1 }; //swapping the orientation
         }
         
-
+        /// <summary>
+        /// Function to add lines of triangle together except for the one to which we consider perpendicular rays
+        /// </summary>
+        /// <param name="e1"></param>
+        /// <param name="e2"></param>
+        /// <param name="e3"></param>
+        /// <returns></returns>
         public static (Line, List<Line>) PutLinesTogether(Edge e1, Edge e2, Edge e3)
         {
             var result = new List<Line>();
@@ -534,6 +602,13 @@ namespace VizualizerWPF
 
             return (e1.lines[0], result);
         }
+
+        /// <summary>
+        /// Iterator to get all the line of the edges incident to <c>v</c>.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="graphCoordinates"></param>
+        /// <returns></returns>
 
         public static IEnumerable<Line> GetEdges(Vertex v, GraphCoordinates graphCoordinates)
         {
