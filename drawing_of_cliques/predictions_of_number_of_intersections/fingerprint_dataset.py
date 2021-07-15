5308175b146e1488abcf7cf8601f8126b7db0556
fila <filip.cermak2@gmail.com>
2021-07-15 10:07:10 +0200

    nicening code

--- drawing_of_cliques/predictions_of_number_of_intersections/fingerprint_dataset.py
+++ drawing_of_cliques/predictions_of_number_of_intersections/fingerprint_dataset.py
@@ -11,6 +11,9 @@ def shuffle_respectively(a, b):
 
 class Dataset:
 
+    train_constant = 0.8
+    dev_constant = 0.9
+
     def __init__(self, args, size):
         self._size = size
         self._dataset = dict()
@@ -30,16 +33,12 @@ class Dataset:
                 self._fingerprints.append([[int(c)] for c in splitted_line[0]])
                 self._intersections.append(int(splitted_line[1]))
 
-        #print(tf.shape(self._fingerprints))
-        #print(tf.shape(self._intersections))
-        #print(self._intersections)
-        #sys.exit()
 
     def set_datasets(self):
         datalen = len(self._fingerprints)
-        self._dataset['train'] = tf.data.Dataset.from_tensor_slices((self._fingerprints[:int(0.8*datalen)], self._intersections[:int(0.8*datalen)])).batch(self._args.batch_size)
-        self._dataset['dev'] = tf.data.Dataset.from_tensor_slices((self._fingerprints[int(0.8*datalen):int(0.9*datalen)], self._intersections[int(0.8*datalen):int(0.9*datalen)])).batch(self._args.batch_size)
-        self._dataset['test'] = tf.data.Dataset.from_tensor_slices((self._fingerprints[int(0.9*datalen):], self._intersections[int(0.9*datalen):])).batch(self._args.batch_size)
+        self._dataset['train'] = tf.data.Dataset.from_tensor_slices((self._fingerprints[:int(train_constant*datalen)], self._intersections[:int(train_constant*datalen)])).batch(self._args.batch_size)
+        self._dataset['dev'] = tf.data.Dataset.from_tensor_slices((self._fingerprints[int(train_constant*datalen):int(dev_constant*datalen)], self._intersections[int(train_constant*datalen):int(dev_constant*datalen)])).batch(self._args.batch_size)
+        self._dataset['test'] = tf.data.Dataset.from_tensor_slices((self._fingerprints[int(dev_constant*datalen):], self._intersections[int(dev_constant*datalen):])).batch(self._args.batch_size)
         
         self.print_test()
         #print(tf.shape(self._dataset['train']))
@@ -58,8 +57,8 @@ class Dataset:
     def get_intersections(self, name):
         datalen = len(self._fingerprints)
         if name == 'train':
-            return self._intersections[:int(0.8*datalen)]
+            return self._intersections[:int(train_constant*datalen)]
         elif name == 'dev':
-            return self._intersections[int(0.8*datalen):int(0.9*datalen)]
+            return self._intersections[int(train_constant*datalen):int(dev_constant*datalen)]
         else:
-            return self._intersections[int(0.9*datalen):]
+            return self._intersections[int(dev_constant*datalen):]
